d70ec89c6c044c24a859808a277eb227d0d56df5
liliana1717 <liliana.monterroza18@gmail.com>
2024-05-15 23:21:13 +0200

    feat:creación de casos de uso para dar de baja a un usuario

--- src/main/java/org/com/iesam/digitallibrary/domain/DeleteUserUseCase.java
+++ src/main/java/org/com/iesam/digitallibrary/domain/DeleteUserUseCase.java
@@ -1,4 +1,22 @@
 package org.com.iesam.digitallibrary.domain;
 
-public class DeleteUserUseCase {
+import org.com.iesam.digitallibrary.data.UserDataRepository;
+
+public class DeleteUserUseCase{
+
+
+    public DeleteUserUseCase(UserRepository userRepository) {
+
+
+    }
+
+    public DeleteUserUseCase(UserDataRepository userDataRepository) {
+    }
+
+    public void execute (String UserName){
+        UserRepository.deleteUser(UserName);
+    }
+
 }
+
+
---
+++ src/main/java/org/com/iesam/digitallibrary/presentation/UserPresentation.java
@@ -0,0 +1,4 @@
+package org.com.iesam.digitallibrary.presentation;
+
+public class UserPresentation {
+}
