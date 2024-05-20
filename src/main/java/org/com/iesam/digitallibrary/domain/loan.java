bb53296d9d9959b92df75d271cc0feb650df355f
liliana1717 <liliana.monterroza18@gmail.com>
2024-05-20 16:33:34 +0200

    feat: creada la clase prestamo

---
+++ src/main/java/org/com/iesam/digitallibrary/domain/loan.java
@@ -0,0 +1,58 @@
+package org.com.iesam.digitallibrary.domain;
+
+public class loan {
+    public Integer bookId;
+    public String userId;
+
+    public String loanDate;
+    public Integer loanId;
+public String returnDate;
+
+    public loan(Integer bookId, String userId, String loanDate, Integer loanId, String returnDate) {
+        this.bookId = bookId;
+        this.userId = userId;
+        this.loanDate = loanDate;
+        this.loanId = loanId;
+        this.returnDate = returnDate;
+    }
+
+    public Integer getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Integer bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getLoanDate() {
+        return loanDate;
+    }
+
+    public void setLoanDate(String loanDate) {
+        this.loanDate = loanDate;
+    }
+
+    public Integer getLoanId() {
+        return loanId;
+    }
+
+    public void setLoanId(Integer loanId) {
+        this.loanId = loanId;
+    }
+
+    public String getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(String returnDate) {
+        this.returnDate = returnDate;
+    }
+}
